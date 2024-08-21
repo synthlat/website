340fee5cf976511c854df7a077f400703357d9f3
RonaldZav <ronaldzavaletapc@gmail.com>
2024-08-20 23:38:25 -0500

    first version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,js,jsx,ts,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
